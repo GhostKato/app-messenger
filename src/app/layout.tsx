d310eed64caadc7884f7978a830bef82d786a6d2
GhostKato <osadchenko.maksym@gmail.com>
2025-01-19 02:38:01 +0200

    edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import './globals.css';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body       
@@ -14,4 +9,6 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+};
+export default RootLayout;
+
