1101620f35672d063944ea0f8acc829bbe4e1b8f
GhostKato <osadchenko.maksym@gmail.com>
2025-01-20 00:58:05 +0200

    edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './globals.css';
 
+export const metadata = {
+  title: 'App messenger',
+  description: 'This is the root layout.',
+  icons: {
+    icon: '/favicon.png',    
+  },
+};
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
