e1d3c7c418400d3416feb8369056971881688eaf
GhostKato <osadchenko.maksym@gmail.com>
2025-01-21 02:40:17 +0200

    add-fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import './globals.css';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap',
+});
 
 export const metadata = {
   title: 'App messenger',
   description: 'This is the root layout.',
   icons: {
-    icon: '/favicon.png',    
+    icon: '/favicon.png',
+  },
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.example.com/',
+    title: 'App messenger',
+    description: 'This is the root layout.',
+    site_name: 'App messenger',
   },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body       
-      >
+    <html lang="en" className={roboto.className}>
+      <body>
         {children}
       </body>
     </html>
   );
 };
-export default RootLayout;
 
+export default RootLayout;
