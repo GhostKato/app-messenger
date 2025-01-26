51471d7c0cf5353ca458fd662268a8caa707559f
GhostKato <osadchenko.maksym@gmail.com>
2025-01-26 22:20:41 +0200

    fixed-style

--- src/app/message/[id]/page.tsx
+++ src/app/message/[id]/page.tsx
@@ -4,22 +4,22 @@ import MessageText from '@/components/MessageText';
 import NewMessage from '@/components/NewMessage';
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import { FaArrowLeft } from "react-icons/fa";
 
 const Message: React.FC = () => {  
-
   const messages = [
-    { id: 1, text: 'Mark Deimonорпопрорполролр парарапр праерара авпрпавпр', from: '1', for: '2' },
-    { id: 2, text: 'Mark Deimon парапрапра рпарапрпа авпрапрапр павпрапрап', from: '2', for: '1' },
-    { id: 3, text: 'Mark Deimon павпвап павпавпв пвапавпав пвапавпавп', forom: '1', for: '2' },
-    { id: 4, text: 'Mark Deimon пвапавпвапвапвапвапва', forom: '2', for: '1' },
-    { id: 5, text: 'Mark Deimonорпопрорполролр парарапр праерара авпрпавпр', from: '1', for: '2' },
-    { id: 6, text: 'Mark Deimon парапрапра рпарапрпа авпрапрапр павпрапрап', from: '1', for: '2' },
-    { id: 7, text: 'Mark Deimon павпвап павпавпв пвапавпав пвапавпавп', forom: '2', for: '1' },
-    { id: 8, text: 'Mark Deimon пвапавпвапвапвапвапва', forom: '1', for: '2' },
-    { id: 9, text: 'Mark Deimonорпопрорполролр парарапр праерара авпрпавпр', from: '1', for: '2' },
-    { id: 10, text: 'Mark Deimon парапрапра рпарапрпа авпрапрапр павпрапрап', from: '2', for: '1' },
-    { id: 11, text: 'Mark Deimon павпвап павпавпв пвапавпав пвапавпавп', forom: '1', for: '2' },
-    { id: 12, text: 'Mark Deimon пвапавпвапвапвапвапва', forom: '2', for: '1' },
+    { id: 1, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '1', for: '2' },
+    { id: 2, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '2', for: '1' },
+    { id: 3, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '1', for: '2' },
+    { id: 4, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '2', for: '1' },
+    { id: 5, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '1', for: '2' },
+    { id: 6, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '1', for: '2' },
+    { id: 7, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '2', for: '1' },
+    { id: 8, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '1', for: '2' },
+    { id: 9, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '1', for: '2' },
+    { id: 10, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '2', for: '1' },
+    { id: 11, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '1', for: '2' },
+    { id: 12, text: 'She decided to pursue her career in law despite the challenges ahead.', from: '2', for: '1' },
   ];
 
   const router = useRouter();
@@ -29,10 +29,12 @@ const Message: React.FC = () => {
   };
 
   return (
-    <div className='flex flex-col relative bg-cool-gradient bg-[length:200%_200%] animate-gradient-move min-h-screen shadow-custom-inset'>
-      <Button variant="back" onClick={handleBackClick}>Back</Button>
-      <h1 className="text-3xl font-bold text-center p-8">Messages</h1>
-      <ul className='bg-three flex flex-col flex-grow gap-[30px] overflow-auto p-[10px]'>
+    // замінити фон
+    <div className='flex flex-col relative shadow-custom-inset bg-three min-h-screen'> 
+    {/* ///////////   */}
+      <Button variant="back" onClick={handleBackClick}><FaArrowLeft className='text-four group-hover:text-white w-[25px] h-[25px] md:w-[35px] md:h-[35px] xl:w-[45px] xl:h-[45px]'/></Button>
+      <h1 className="bg-cool-gradient bg-[length:200%_200%] animate-gradient-move text-3xl font-bold text-center p-8">Messages</h1>
+      <ul className='flex flex-col flex-grow gap-[30px] overflow-auto p-[10px]'>
         {messages.map((message) => (
           <MessageText key={message.id} message={message.text} from={message.from} />
         ))}        
