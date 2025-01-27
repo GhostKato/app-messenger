47803d0e4c5fa5074ef534916ecf5e063891481d
GhostKato <osadchenko.maksym@gmail.com>
2025-01-27 21:33:59 +0200

    edit-styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className='home-container flex flex-col justify-center items-center gap-10 bg-cool-gradient bg-[length:200%_200%] animate-gradient-move min-h-screen'>
+    <div className='flex flex-col justify-center items-center gap-10 bg-cool-gradient bg-[length:200%_200%] animate-gradient-move min-h-screen'>
       <h1 className='text-[30px] md:text-[50px]'>App messenger</h1>
       <div className='rounded-full overflow-hidden w-[250px]  md:w-[450px] xl:w-[500px] border-4 border-interaction shadow-custom'>
         <Image                    
--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -14,20 +14,20 @@ const UserMenu: React.FC = () => {
     };
 
     return (
-        <div className="user-menu-container bg-sidebar w-[200px] h-[470px] rounded-lg flex flex-col gap-4 pt-[50px] pl-5 pr-5 border border: border-border shadow-custom">
+        <div className="absolute top-[20px] left-[20px] z-[100] bg-sidebar w-[200px] h-[470px] rounded-lg flex flex-col gap-4 pt-[50px] pl-5 pr-5 border border-border shadow-custom">
             <Button variant="close" onClick={handleUserMenuToggle}>
                 <IoCloseOutline className="w-6 h-6 md:w-8 md:h-8 xl:w-10 xl:h-10" />
             </Button>            
-            <div className='flex flex-col gap-2'>               
+            <div className="flex flex-col gap-2">               
                 <ThemeSelect type="sidebar" />
-                 <ThemeSelect type="receiving"/>
+                <ThemeSelect type="receiving" />
                 <ThemeSelect type="interaction" />
                 <ThemeSelect type="image" />
             </div>
             <UserMenuItem pathname="/message/">
                 Edit user
-        </UserMenuItem>     
-            </div>
+            </UserMenuItem>     
+        </div>
     );
 };
 
