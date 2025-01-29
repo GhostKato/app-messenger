75b6d8ec0bbff9abee58bec4135d04fb02b7a6ed
GhostKato <osadchenko.maksym@gmail.com>
2025-01-29 12:22:40 +0200

    fixed

--- src/components/FormMenuItem.tsx
+++ src/components/FormMenuItem.tsx
@@ -10,7 +10,7 @@ export interface FormMenuProps {
 
 const FormMenuItem: React.FC<FormMenuProps> = ({ current, pathname, children }) => {
   const listItemClasses = clsx(
-    'flex justify-center items-center h-[30px] relative text-lg md:text-2xl',
+    'flex justify-center items-center h-[30px] relative text-lg md:text-2xl hover:text-interaction ',
     current && 'text-interaction after:absolute after:bottom-[-6px] after:left-0 after:h-1 after:w-full after:bg-sidebar after:border-interaction after:border-2 after:rounded-full'
   );
 
