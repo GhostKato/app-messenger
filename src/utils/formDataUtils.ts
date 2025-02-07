497e8d61000d57cd4a96334645fc64319d4eee65
GhostKato <osadchenko.maksym@gmail.com>
2025-02-07 18:30:24 +0200

    fixed

--- src/utils/formDataUtils.ts
+++ src/utils/formDataUtils.ts
@@ -1,18 +1,28 @@
 import { UserFormDataType } from "@/types/userTypes";
 
-export const createUserFormData = (body: UserFormDataType): FormData => {
-  const formData = new FormData();  
-  
-  formData.append('name', body.name);
-  formData.append('email', body.email);   
+
+export function createFormData(body: UserFormDataType): FormData {
+  const formData = new FormData();
+
+  if (body.name) {
+    formData.append('name', body.name);
+  }
+
+  if (body.email) {
+    formData.append('email', body.email);
+  }
 
   if (body.password) {
     formData.append('password', body.password);
   }
-       
+
   if (body.photo) {
     formData.append('photo', body.photo);
   }
-  
+
   return formData;
-};
+}
+
+
+
+
